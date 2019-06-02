6fc93e85165a75dd19c8a7b5efbe6d45452b8dcb
aoizora <mathesis@inbox.ru>
2019-06-02 14:13:49 +0300

    Незначительные изменения

--- sample.asm
+++ sample.asm
@@ -1,4 +1,4 @@
-format binary
+format binary as 'exe'
 use32
 
 ;
@@ -35,13 +35,13 @@ sectalign equ 1
 pesig:
   dd "PE"
 filehdr:
-  dw 0x014C  ; Machine i386
-  dw 1       ; NumberOfSections
-  dd 0       ; TimeDateStamp UNUSED
-  dd 0       ; PointerToSymbolTable UNUSED
-  dd 0       ; NumberOfSymbols UNUSED
-  dw 0       ; SizeOfOptionalHeader (including data directories)
-  dw 0x103   ; Characteristics (no relocs, executable, 32-bit)
+  dw 0x014C      ; Machine i386
+  dw 1           ; NumberOfSections
+  dd 0           ; TimeDateStamp UNUSED
+  dd 0           ; PointerToSymbolTable UNUSED
+  dd 0           ; NumberOfSymbols UNUSED
+  dw opthdrsize  ; SizeOfOptionalHeader (including data directories)
+  dw 0x103       ; Characteristics (no relocs, executable, 32-bit)
 opthdr:
   dw 0x10B     ; Magic (PE32)
   db 0         ; MajorLinkerVersion UNUSED
@@ -63,7 +63,7 @@ opthdr:
   dw 0         ; MinorSubsystemVersion
   dd 0         ; Win32VersionValue UNUSED
   dd 0         ; SizeOfImage
-  dd 0         ; SizeOfHeaders
+  dd hdrsize   ; SizeOfHeaders
   dd 0         ; CheckSum UNUSED
   dw 2         ; Subsystem (Win32 GUI)
   dw 0x400     ; DllCharacteristics UNUSED
@@ -74,6 +74,9 @@ opthdr:
   dd 0         ; LoaderFlags UNUSED
   dd 16        ; NumberOfRvaAndSizes UNUSED
 
+opthdrsize = $ - opthdr
+hdrsize = $ - $$
+
 code:
 ;
 ; Entry Point
@@ -83,5 +86,5 @@ start:
   pop eax
   ret
 
-codesize equ $ - code
-filesize equ $ - $$
+codesize = $ - code
+filesize = $ - $$
